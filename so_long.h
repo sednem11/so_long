b617e415391550db79efafadb13536de45e37991
Mateus Campos Mendes <macampos@c2r2s4.42lisboa.com>
2024-03-05 14:02:57 +0000

    so_long finished with 125 bonus

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: macampos <macampos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 18:48:24 by macampos          #+#    #+#             */
-/*   Updated: 2024/03/04 19:42:38 by macampos         ###   ########.fr       */
+/*   Updated: 2024/03/05 14:01:14 by macampos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # define SCALE2 32
 # define SCALE3 32
 
-typedef struct	s_image
+typedef struct s_image
 {
 	void	*img;
 	char	*image_pixel;
@@ -71,22 +71,22 @@ void	create_image_addr(t_image *image);
 void	get_xy(int fd);
 void	map(int fd);
 void	floodfill(char **map, int x, int y);
-int		player_colectable_exit();
-void	check_if_squared();
-void	mlx_start();
-void	player_cordinates();
+int		player_colectable_exit(void);
+void	check_if_squared(void);
+void	mlx_start(void);
+void	player_cordinates(void);
 void	print_map(char **map);
-void	mlx_end(int	esc);
+void	mlx_end(int esc);
 void	rendering_map(int move);
-void	my_pixel_put(t_image **image, int x , int y, int color);
+void	my_pixel_put(t_image **image, int x, int y, int color);
 void	create_image_ptr(t_image **image, char *file);
-void	create_images();
-void	change_player(int	direction);
-void	player_colectable_exit2();
+void	create_images(void);
+void	change_player(int direction);
+void	player_colectable_exit2(void);
 void	end_game(int direction, int x, int y);
 void	end_game2(int direction, int x, int y);
 void	rendering(t_image **image, int positionx, int positiony, int i);
-void	move_count();
+void	move_count(void);
 void	change_player(int direction);
 void	check_exit(int y, int x);
 void	init_mlx_images(void);
